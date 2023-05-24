7f4195c8e6253979700e5f4416fe8791172afc31
snow3all <Netithorn_rly@hotmail.com>
2023-05-24 13:20:56 +0700

    [DONE] Auth Module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthRegister } from './dto/authRegister.dto';
+import { AuthLogin } from './dto/authLogin.dto';
 import { Response } from 'express';
 
 @Controller()
@@ -10,5 +11,10 @@ export class AppController {
   @Post('/auth/register')
   registerUser(@Body() body: AuthRegister, @Res() res: Response) {
     return this.appService.registerUser(body, res);
+  }
+
+  @Post('/auth/login')
+  loginUser(@Body() body: AuthLogin, @Res() res: Response) {
+    return this.appService.loginUser(body, res);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { AuthRegister } from './dto/authRegister.dto';
+import { AuthLogin } from './dto/authLogin.dto';
 import { Response } from 'express';
 
 @Injectable()
@@ -11,6 +12,23 @@ export class AppService {
     try {
       const dataFormMicroservice = await this.http.axiosRef.post(
         `${process.env.AUTH_URL}/register`,
+        { data: body },
+      );
+      console.log('dataFormMicroservice: ', dataFormMicroservice);
+      return res.status(200).json({
+        data: dataFormMicroservice.data,
+      });
+    } catch (e) {
+      return res.status(500).json({
+        message: e,
+      });
+    }
+  }
+
+  async loginUser(body: AuthLogin, res: Response) {
+    try {
+      const dataFormMicroservice = await this.http.axiosRef.post(
+        `${process.env.AUTH_URL}/login`,
         { data: body },
       );
       console.log('dataFormMicroservice: ', dataFormMicroservice);
